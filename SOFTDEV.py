7f73d0cc641400b0bdad304117c099c367b8a48f
Lifehacker000 <99037990+Lifehacker000@users.noreply.github.com>
2022-09-24 23:58:59 +0530

    Create SOFTDEV.py

---
+++ SOFTDEV.py
@@ -0,0 +1,26 @@
+
+n = int(input())
+star=[""]
+for i in range(n):
+  star[i]=(input())
+
+n0=["zero","one","two","three","four","five","six","seven","eight","nine"]
+for i in range(10):
+  st2= n0[i]
+  st3= sorted(st2)
+  #print (st3)
+
+st8=[""]
+for i in range(n):
+  st = star[i]
+  st1= sorted(st)
+  st8[i] = st1
+  #print (st1)
+  #pass the sorted array of n0 as argument and find z first and remove it from string if we find
+  #another z then again remove z from string
+  #if (n0[i]):"""
+
+    
+
+
+
